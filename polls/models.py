8e7a8aa5f140e9baf50a9b29c4a191feece49bf5
louiedweck <dweck.louis@gmail.com>
2020-02-06 23:16:19 -0800

    updated index method

--- polls/models.py
+++ polls/models.py
@@ -13,6 +13,7 @@ class Question(models.Model):
 
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    objects = models.Manager()
 
 
 class Choice(models.Model):
